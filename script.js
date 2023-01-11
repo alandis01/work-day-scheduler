ae8abf09a6b5c69121a94d282812ce19d0a48172
ashley <alandis01@gmail.com>
2023-01-11 09:14:44 -0500

    moved save button and current day to top of JS file, deleted commented out comments in index.html file

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+var today = today();
+
+$(document).ready(function(){
+
+
+});
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
 
-var current_hour = dayjs().get("hour")
-console.log (current_hour)
+// var current_hour = dayjs().get("hour");
+// console.log (current_hour);
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -12,12 +21,12 @@ console.log (current_hour)
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  $('.btn').on('click', function(){
-   var key = $(this).parent().attr('id')
-   var textData = $(this).siblings('textarea').val()
 
-   localStorage.setItem(key, textData)
-  })
+  // $('.btn').on('click', function(){
+  //  var key = $(this).parent().attr('id')
+  //  var textData = $(this).siblings('textarea').val()
+  //  localStorage.setItem(key, textData)
+  // });
     
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -26,13 +35,23 @@ console.log (current_hour)
   // current hour in 24-hour time?
   //
 
+  // var currentTime = function() {
+  //   var currentHour = moment().hour()
+  //   for (var i=8; i<18; i++)
+  //   if(currentHour>i){
+  //     $
+  //   }
+
+  // }
+
   
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-  $('#currentDay').text(dayjs().format("DD/MM/YYYY"))
+
+  // $('#currentDay').text(dayjs().format("DD/MM/YYYY"))
 
 // document.getElementById('hour-12').classList.add('') // do things with this
 // // wrap in a function
