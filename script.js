3d45bad43a5e4f7fe84da2a5e3cee4976757be34
ashley <alandis01@gmail.com>
2023-01-14 16:37:47 -0500

    date is now displaying on page, past future and present now displaying

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-var today = now();
-
 $(document).ready(function () {
-  $("#currentDay").text(today().format("dddd, MMM Do"));
+  $("#currentDay").text(moment().format("dddd, MMM Do"));
   $(".saveBtn").on("click", function () {
     var scriptText = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
@@ -19,63 +17,39 @@ $(document).ready(function () {
   $("#hour17 .description").val(localStorage.getItem("17"));
 
   function changeTime() {
-    var currentHour = now().hour();
+    var currentHour = moment().hour();
 
-    $('.time-block').each(function() {
-      var idTime = parseInt($(this).attr("id"));
+    $('.time-block').each(function () {
+      var idTime = parseInt(this.id.split("-")[1]);
+      console.log(this.id.split("-")[1])
       if (idTime < currentHour) {
         $(this).addClass("past");
       } else if (idTime === currentHour) {
         $(this).addClass("present");
-        $(this).addClass("past");
+        $(this).removeClass("past");
       } else {
         $(this).addClass("future");
         $(this).removeClass("past");
         $(this).removeClass("present");
       }
     })
-}
-
-changeTime()
-var timeInterval = setInterval(timeChange, 1000);
-
+  }
+  changeTime()
 });
 
-
-var current_hour = dayjs().get("hour");
-
-
-  // $('.btn').on('click', function(){
-  //  var key = $(this).parent().attr('id')
-  //  var textData = $(this).siblings('textarea').val()
-  //  localStorage.setItem(key, textData)
-  // });
-
-
-  var currentTime = function() {
-    var currentHour = moment().hour()
-    for (var i=8; i<18; i++)
-    if(currentHour>i){
+var currentTime = function () {
+  var currentHour = moment().hour()
+  for (var i = 8; i < 18; i++)
+    if (currentHour > i) {
       $
     }
 
-  }
+};
 
-  $('#currentDay').text(dayjs().format("DD/MM/YYYY"))
+$('#currentDay').text(moment().format("DD/MM/YYYY"));
+$('.description').each(function () {});
 
-// document.getElementById('hour-12').classList.add('') // do things with this
-// wrap in a function
-var timeValue = dayjs
-$('.description').each(function(){
-  if (timeValue < 'hour-11') { // use the parent and/or siblings methods from jQuery
-    // execute this code block
-  }
-})
-
-// $();
-function rowTimeBlock (){
-  if (rowTimeBlock = true); 
-  display 
-  // changing the colors of my scheduler past present and future
-
+function rowTimeBlock() {
+  if (rowTimeBlock = true);
+  display
 }
